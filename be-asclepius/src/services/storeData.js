8ba2acbc018b88d28a7c18366718477b2fdbcfdc
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-11-22 08:37:42 +0700

    submissionmlgc

---
+++ be-asclepius/src/services/storeData.js
@@ -0,0 +1,12 @@
+const { Firestore } = require('@google-cloud/firestore');
+
+function storeData(id, data) {
+  const db = new Firestore({
+    projectId: 'submissionmlgc-aziz',
+  });
+
+  const predictCollection = db.collection('predictions');
+  return predictCollection.doc(id).set(data)
+}
+
+module.exports = storeData;
